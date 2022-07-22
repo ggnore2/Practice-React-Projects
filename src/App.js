41400f12042eaac434abdea4b18168a34c8b2702
L + Ratio <who asked>
2022-07-22 23:19:58 +0700

    Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import datas from "./data.js";
+
+function moveLeft(index,length){
+  index -= 1;
+  if(index < 0){
+    index = length - 1 ;
+  }
+  return index;
+}
+
+function moveRight(index,length){
+  index += 1;
+  if(index >= length){
+    index = 0 ;
+  }
+  return index;
+}
+let index = 0 ;
+let length = datas.length;
+
+function Data(){
+  const [state,setState] = useState(datas[index]);
+  async function leftClick(){
+    index = await moveLeft(index,length);
+    await setState(datas[index]);
+  }
+  async function rightClick(){
+    index = moveRight(index,length);
+    await setState(datas[index]);
+  }
+  return <>
+      <div className = "Picture">
+        <img src = {state.image} alt = "image"/>
+      </div>
+      <div className = "Name">{state.name}</div>
+      <div className = "Age">Age : {state.age}</div>
+      <div className="Buttons">
+        <button className = "left" onClick={leftClick}>{"<"}</button>
+        <button className = "right" onClick={rightClick}>{">"}</button>
+      </div>
+  </>;
+}
+
+function Card(props){
+    return <div className = "Card">
+      <Data/>
+    </div> ;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "Background">
+      <Card/>
     </div>
   );
 }
